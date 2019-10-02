df08b16a7ce5561a215063e76334cfb85e4c10a5
hurrellt <hurrelltomas@gmail.com>
2019-10-01 21:10:08 -0300

    Preloading users into the db

--- src/main/java/app/ViandasYaApplication.java
+++ src/main/java/app/ViandasYaApplication.java
@@ -1,12 +1,37 @@
 package app;
 
+import app.api.user.UserRepository;
+import app.model.User.Client.ClientFactory;
+import app.model.User.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class ViandasYaApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(ViandasYaApplication.class, args);
+    }
+}
+
+@Component
+class DBPreloader {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @EventListener
+    public void appReady(ApplicationReadyEvent event) {
+        //Users to Preload
+        User tomasHurrell = ClientFactory.tomasHurrell();
+        User federicoMartinez = ClientFactory.federicoMartinez();
+
+        userRepository.save(tomasHurrell);
+        userRepository.save(federicoMartinez);
     }
 }
